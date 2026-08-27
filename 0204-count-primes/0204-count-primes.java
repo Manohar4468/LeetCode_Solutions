de24b3663012b0d1b15b0524ac612d4f58f98ee5
Gudibandi Manohar Reddy <gudibandimanoharreddy@gmail.com>
2026-08-27 18:50:36 +0530

    Time: 759 ms (6.53%), Space: 79.7 MB (11.87%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    public int countPrimes(int n) {
+        boolean[] arr=new boolean[n];
+        
+        Arrays.fill(arr,true);
+        
+        for(int i=2;i*i<n;i++)
+        {
+            if(arr[i]==true)
+            {
+                for(int j=i*i;j<n;j+=i)
+                {
+                    arr[j]=false;
+                }
+            }
+        }        
+        int cnt=0;
+        for(int i=2;i<n;i++)
+        {
+            if(i>=n)
+            {
+                break;
+            }
+            else
+            {
+                if(arr[i]==true)
+                {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
