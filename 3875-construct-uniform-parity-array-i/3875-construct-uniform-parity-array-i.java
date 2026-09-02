a3812265ab852673c3abd148f883390c3ba20120
Gudibandi Manohar Reddy <gudibandimanoharreddy@gmail.com>
2026-09-02 19:08:17 +0530

    Time: 0 ms (100%), Space: 45.3 MB (16.73%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        /*
+        ->if all elements in nums1 are even i will simply return true;
+        ->if nums1 contain atleast one odd i will make all elemenst as odd (even-odd=odd)
+        */
+        return true;
+    }
+}
