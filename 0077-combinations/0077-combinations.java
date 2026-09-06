5bb8b3343e4f210eb63de76eff042d7bfb9d7772
Gudibandi Manohar Reddy <gudibandimanoharreddy@gmail.com>
2026-09-06 12:02:14 +0530

    Time: 29 ms (6.13%), Space: 97.7 MB (95.15%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static List<List<Integer>> combination(int index, int[] nums, List<Integer> temp, List<List<Integer>> ans,int k)
+    {
+        if(index==nums.length)
+        {
+            if(temp.size()==k)
+            {
+                ans.add(new ArrayList<>(temp));
+            }
+            return ans;
+        }
+        //pick
+        temp.add(nums[index]);
+        combination(index+1,nums,temp,ans,k);
+        temp.remove(temp.size()-1);
+        combination(index+1,nums,temp,ans,k);
+        //non-pick
+        return ans;
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        int[] nums= new int[n];
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=i+1;
+        }
+        List<Integer> temp= new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
+        return combination(0,nums,temp,ans,k);
+    }
+}
