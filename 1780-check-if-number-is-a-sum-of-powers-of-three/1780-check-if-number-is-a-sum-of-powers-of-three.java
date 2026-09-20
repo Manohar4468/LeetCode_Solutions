d7e25f6137a82b6b02e0ed4d8076b5985432cbb1
Gudibandi Manohar Reddy <gudibandimanoharreddy@gmail.com>
2026-09-20 22:27:43 +0530

    Time: 0 ms (100%), Space: 42.2 MB (29.94%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.java
@@ -3,14 +3,8 @@ class Solution {
         
         while(n>0)
         {
-            if(n%3==2)
-            {
-                return false;
-            }
-            else
-            {
-                n=n/3;
-            }
+            if(n%3==2) return false;
+            else n=n/3;
         }
         return true;
     }
